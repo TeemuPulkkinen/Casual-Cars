ebbb2bb8f867389cbb6e686d452cb6117ebe71a9
Devousa <pulkkinen.teemu@gmail.com>
2019-11-22 03:54:43 +0200

    Added a scrolling background. Start of UI building.

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSpeedX; // value of the background's transform's x-coordinate
+
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSpeedX);
+        transform.position = startPosition + Vector3.left * newPosition;
+    }
+}
