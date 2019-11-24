359eb7c833751b90820587f1d382b0f38917b489
Devousa <pulkkinen.teemu@gmail.com>
2019-11-24 21:18:15 +0200

    Final commit 24th Nov 2019

---
+++ Assets/Scripts/Blocker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocker : MonoBehaviour
+{
+    public float blockerSpeed;
+    public Rigidbody2D rb2d;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = -transform.right * blockerSpeed; 
+    }
+
+}
